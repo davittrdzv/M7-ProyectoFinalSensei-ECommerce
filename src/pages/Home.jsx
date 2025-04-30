be6a86bd3102abc2e006314e0e72dacb568505a5
davittrdzv <davittrdzv@gmail.com>
2025-04-29 18:42:01 -0600

    Commit 5:
    1.	.gitignore: Respecto del archivo database.json (que solo es para pruebas mientras no utilice la API local).
    2.	ProductCardHome.jsx: Creación de props. Ajuste visual en className.
    3.	Styles.css: Cödigo para ajuste visual adicional de ProductCardHome.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,21 @@
 import ProductCardHome from '../components/ProductCardHome.jsx'
+import database from '../assets/database.json'
 
 const Home = () => {
+  const products = database.items
   return (
-    <div className='border-top border-danger mt-4_0rem'>
-      <h1>Home</h1>
+    <div className='container border-top border-danger mt-4_0rem'>
+      <h1 className='text-center'>Home</h1>
       <div className='row row-cols-1 row-cols-md-4 g-4'>
-        <ProductCardHome />
+        {products.map(product => (
+          <ProductCardHome
+            image={product.image || product.images || 'https://picsum.photos/200'}
+            key={product.sku}
+            name={product.product_name}
+            description={product.description}
+            price={product.price}
+          />
+        ))}
       </div>
     </div>
   )
