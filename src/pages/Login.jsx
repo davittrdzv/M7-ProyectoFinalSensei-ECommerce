55a3c0608f3de070b1b997225e56a41082f65552
davittrdzv <davittrdzv@gmail.com>
2025-05-08 13:41:03 -0600

    Commit 15:
    1.	form.css: Creación de hoja de estilos específicos para forms de Log In y Sign Up.
    2.	Login.jsx: Estructura html.
    3.	SignUp.jsx: Estructura html.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,43 @@
+import '@/styles/form.css'
+import { Link } from 'react-router-dom'
+
 const Login = () => {
   return (
-    <div className='container border-top border-danger mt-4_0rem'> {/* border-top border-danger son para referencia */}
-      <h1>Login</h1>
+    <div className='border-top border-danger mt-4_0rem'>
+      <main className='form-signin w-100 m-auto'>
+        <form>
+          <h3 className='mb-3 fw-normal text-center'>Hi Guest!</h3>
+          <p className='mb-3 fw-normal text-center'>Type your E-Mail and Password to Log In</p>
+          <div className='form-floating'>
+            <input
+              type='email'
+              className='form-control email-login'
+              placeholder='E-Mail'
+              id='email'
+              name='email'
+            /><label htmlFor='email'>E-Mail</label>
+          </div>
+          <div className='form-floating'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              placeholder='Password'
+            /><label htmlFor='password'>Password</label>
+          </div>
+          <button
+            className='btn btn-custom-gold w-100 py-2'
+            type='submit'
+          >
+            Log In
+          </button>
+          <p className='mt-3 fw-normal text-center'>
+            <span>Don't have an account? <Link to='/signup'>Click here</Link></span>
+            <span> to Sign Up.</span>
+          </p>
+        </form>
+      </main>
     </div>
   )
 }
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,67 @@
+import '@/styles/form.css'
+
 const SignUp = () => {
   return (
-    <div className='container border-top border-danger mt-4_0rem'> {/* border-top border-danger son para referencia */}
-      <h1>Signup</h1>
+    <div className='border-top border-danger mt-4_0rem'>
+      <main className='form-signin w-100 m-auto'>
+        <form>
+          <h1 className='h3 mb-3 fw-normal text-center'>Create your Account</h1>
+          <div className='form-floating'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='First Name'
+              id='first_name'
+              name='first_name'
+            /><label htmlFor='first_name'>First Name</label>
+          </div>
+          <div className='form-floating'>
+            <input
+              type='text'
+              className='form-control'
+              placeholder='Last Name'
+              id='last_name'
+              name='last_name'
+            /><label htmlFor='last_name'>Last Name</label>
+          </div>
+          <div className='form-floating'>
+            <select
+              className='form-select'
+              id='gender'
+              name='gender'
+            >
+              <option value='' disabled>Select your Gender</option> {/* No se regresa a este cuando hago refresh */}
+              <option value='M'>Male</option>
+              <option value='F'>Female</option>
+            </select>
+            <label htmlFor='gender'>Gender</label>
+          </div>
+          <div className='form-floating'>
+            <input
+              type='email'
+              className='form-control email-signup'
+              placeholder='E-Mail'
+              id='email'
+              name='email'
+            /><label htmlFor='email'>E-Mail</label>
+          </div>
+          <div className='form-floating'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              placeholder='Password'
+            /><label htmlFor='password'>Password</label>
+          </div>
+          <button
+            className='btn btn-custom-gold w-100 py-2'
+            type='submit'
+          >
+            Sign Up
+          </button>
+        </form>
+      </main>
     </div>
   )
 }
