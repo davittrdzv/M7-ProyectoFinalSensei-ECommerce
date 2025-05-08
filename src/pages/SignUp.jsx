641cbc8615d8c56d72e6d2449a40d39653b3dead
davittrdzv <davittrdzv@gmail.com>
2025-05-08 17:40:15 -0600

    Commit 19:
    1.	SignUp.jsx: Código básico con base en react-hook-form para registro de datos de nuevo usuario, validación de campos requeridos y limpieza de formulario una vez enviado.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,17 @@
 import '@/styles/form.css'
+import { useForm } from 'react-hook-form'
 
 const SignUp = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm()
+  const onSubmit = data => {
+    console.log(data)
+    reset()
+  }
+
   return (
     <div className='border-top border-danger mt-4_0rem'>
       <main className='form-signin w-100 m-auto'>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <h1 className='h3 mb-3 fw-normal text-center'>Create your Account</h1>
           <div className='form-floating'>
             <input
@@ -13,7 +20,9 @@ const SignUp = () => {
               placeholder='First Name'
               id='first_name'
               name='first_name'
+              {...register('first_name', { required: true })}
             /><label htmlFor='first_name'>First Name</label>
+            {errors.first_name && <p className='text-center mb-0'>This field is required</p>}
           </div>
           <div className='form-floating'>
             <input
@@ -22,19 +31,24 @@ const SignUp = () => {
               placeholder='Last Name'
               id='last_name'
               name='last_name'
+              {...register('last_name', { required: true })}
             /><label htmlFor='last_name'>Last Name</label>
+            {errors.last_name && <p className='text-center mb-0'>This field is required</p>}
           </div>
           <div className='form-floating'>
             <select
               className='form-select'
               id='gender'
               name='gender'
+              defaultValue=''
+                            {...register('gender', { required: true })}
             >
               <option value='' disabled>Select your Gender</option> {/* No se regresa a este cuando hago refresh */}
               <option value='M'>Male</option>
               <option value='F'>Female</option>
             </select>
             <label htmlFor='gender'>Gender</label>
+            {errors.gender && <p className='text-center mb-0'>This field is required</p>}
           </div>
           <div className='form-floating'>
             <input
@@ -43,16 +57,20 @@ const SignUp = () => {
               placeholder='E-Mail'
               id='email'
               name='email'
+              {...register('email', { required: true })}
             /><label htmlFor='email'>E-Mail</label>
+            {errors.email && <p className='text-center mb-0'>This field is required</p>}
           </div>
           <div className='form-floating'>
             <input
               type='password'
               className='form-control'
+              placeholder='Password'
               id='password'
               name='password'
-              placeholder='Password'
+              {...register('password', { required: true })}
             /><label htmlFor='password'>Password</label>
+            {errors.email && <p className='text-center mb-0'>This field is required</p>}
           </div>
           <button
             className='btn btn-custom-gold w-100 py-2'
