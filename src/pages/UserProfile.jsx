1adf06e9f7db6f45da9d46e03ede3a7db7e583ce
davittrdzv <davittrdzv@gmail.com>
2025-05-15 14:06:18 -0600

    Commit 38:
    1.	UserProfile: Estructura HTML para mostrar información del usuario con consumo de contexto AuthProvider.

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,8 +1,49 @@
+import { useAuthContext } from '@/hooks/useAuthContext'
+
 const UserProfile = () => {
+  const { user } = useAuthContext()
   return (
-    <div className='container border-top border-danger mt-4_0rem'> {/* border-top border-danger son para referencia */}
-      <h1 className='text-center'>User Profile</h1>
+    <div className='container border-top border-danger mt-4_0rem'>
+      <h2 className='text-center'>
+        {`Welcome to your Profile Information, ${user.first_name} ${user.last_name}!`}
+      </h2>
+
+      <div className='mx-auto mt-4 user-table-container'>
+        <table className='table table-bordered table-striped mb-5'>
+          <tbody>
+            <tr>
+              <td>First Name</td>
+              <td>{user.first_name}</td>
+            </tr>
+            <tr>
+              <td>Last Name</td>
+              <td>{user.last_name}</td>
+            </tr>
+            <tr>
+              <td>Email</td>
+              <td>{user.email}</td>
+            </tr>
+            <tr>
+              <td>Gender</td>
+              <td>{user.gender === 'M' ? 'Male' : 'Female'}</td>
+            </tr>
+            <tr>
+              <td>Role</td>
+              <td>{user.role}</td>
+            </tr>
+            <tr>
+              <td>Created At</td>
+              <td>{new Date(user.createdAt).toString()}</td>
+            </tr>
+            <tr>
+              <td>Updated At</td>
+              <td>{new Date(user.updatedAt).toString()}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+
   )
 }
 
